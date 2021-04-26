9255ef67d730ff5b4e6ee0480cb7cae4446f1016
matheus-rodrigues-m <matheusrodriguessmaia0318@gmail.com>
2021-04-26 03:20:49 -0300

    Commit Styles #2

--- src/app/components/page/home/home.component.ts
+++ src/app/components/page/home/home.component.ts
@@ -6,6 +6,10 @@ const ELEMENT_DATA: any = [
   { name: 'Macarrão', description: 'Comida gostosa hummmm', preco: 'R$20,00', tipo: 'Refeição', disponibilidade: 'Disponivel' },
   { name: 'Sushi', description: 'Comida gostosa hummmm', preco: 'R$30,00', tipo: 'Refeição', disponibilidade: 'Disponivel'},
   { name: 'Feijoada', description: 'Comida gostosa hummmm', preco: 'R$40,00', tipo: 'Refeição', disponibilidade: 'Indisponivel' },
+  { name: 'Parmegiana', description: 'Bife à parmegiana ou filé à parmegiana, um prato brasileiro, é um tipo de bife frito, composto por um pedaço de carne fatiado, empanado com farinha de trigo e ovos', preco: 'R$40,00', tipo: 'Refeição', disponibilidade: 'Disponivel' },
+  { name: 'Frango Empanado', description: 'Frango empanado é um prato muito comum e apreciado no mundo todo. No Japão é chamado de chicken katsu. Consiste em filés de frango...', preco: 'R$10,00', tipo: 'Refeição', disponibilidade: 'Disponivel' },
+  { name: 'Misto Quente', description: 'Um misto-quente, uma tosta-mista ou uma sandes mista é um sanduíche quente, preparado à base de queijo e presunto. Pode ser preparado em frigideira...', preco: 'R$20,00', tipo: 'Sanduiche', disponibilidade: 'Disponivel'},
+  { name: 'Sanduíche de Frango', description: 'Um sanduíche de frango é um sanduíche que normalmente consiste em peito de frango desossado e sem pele servido entre fatias de pão.', preco: 'R$30,00', tipo: 'Sanduiche', disponibilidade: 'Indisponivel' },
 ];
 
 @Component({
